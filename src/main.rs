139a46bc6f1bf7f9cd81e9868bcdffbec9ec6c94
kelvindoe22 <kelvindoe22@gmail.com>
2022-01-18 11:29:17 +0000

    added -e

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use std::fs;
 use std::env;
+use std::process::Command;
 
 
 
 
 
 fn main() {
-    let f_or_f = env::args()
-                                .skip(1)
-                                .next()
-                                .unwrap_or_else(
-                                    || {
-                                        print!(".");
-                                        std::process::exit(0)
-                                    }
-                                );
+    let mut args = env::args()
+                                .skip(1);
+    let f_or_f = args.next().unwrap_or_else(||
+         {print!(".");
+         std::process::exit(0);}
+        );
+    
+    let exe = args.next();
+    
     let f_or_f = f_or_f.as_str();
     let findchar = f_or_f.rfind('/');
     let a = if findchar.is_none() {
@@ -25,7 +26,29 @@ fn main() {
         let file = &f_or_f[x+1..]; 
         find(dir, file)
     };
-    if a{print!("{}",f_or_f)}else{print!(".")}
+    if !a {
+        print!(".");
+    }else {
+        if exe.is_none() {
+            println!("{}",f_or_f)
+        }else {
+            if exe.unwrap() != String::from("-e") {
+                println!("Expected -e")
+            }else {
+                let app = args.next().unwrap_or_else(||
+                     {print!("expected executable || command found none"); std::process::exit(0)}
+                    );
+                
+                let command = &*format!("{} {}", app, f_or_f);
+                
+                Command::new("powershell")
+                    .args(["/C",command])
+                    .spawn()
+                    .expect("Unexpected error");
+
+            }
+        }
+    }
 }
 
 fn find(folder: &str, f_or_f: &str) -> bool{
@@ -45,3 +68,10 @@ fn find(folder: &str, f_or_f: &str) -> bool{
 }
 
 
+#[test]
+fn test_cmd(){
+    Command::new("powershell")
+                .args(["/C","code ."])
+                .spawn()
+                .expect("siade");
+}
